e3de022d86848d1ddcdcb3df41c384119812ad81
PEEVEE <peeveezz2020@gmail.com>
2022-03-24 20:08:38 +0200

    buttons && interactions

--- src/database.rs
+++ src/database.rs
@@ -1,10 +1,12 @@
 pub struct Allowlist {
-    pub message_id: String,
-    pub author_id: String,
+    pub message_id: u64,
+    pub author_id: u64,
 }
 
 pub fn save_allowlist(data: Allowlist) {}
 
-pub fn get_allowlist(message_id: String) {}
+pub fn get_allowlist_author(message_id: u64) -> u64 {
+    return 936258404920475748; //test
+}
 
-pub fn remove_allowlist(message_id: String) {}
+pub fn remove_allowlist(message_id: u64) {}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ mod database;
 use crate::database::*;
 use serenity::{
     async_trait, client::bridge::gateway::GatewayIntents, model::channel::Message,
-    model::gateway::Ready, model::id::ChannelId, prelude::*,
+    model::gateway::Ready, model::id::ChannelId,
+    model::interactions::message_component::ButtonStyle, model::interactions::Interaction,
+    prelude::*,
 };
 use std::env;
+use tokio::time::{sleep, Duration};
 
 struct Handler;
 
@@ -16,17 +19,20 @@ impl EventHandler for Handler {
     }
     async fn message(&self, ctx: Context, message: Message) {
         if !message.author.bot {
+            // lähetys kanavan id .env tiedostossa
             let send_channel_id = env::var("SEND_CHANNEL")
                 .unwrap()
                 .parse::<u64>()
-                .expect("Lähetys kanavan ID:tä ei löytynyt");
-            let check_channel_id = env::var("CHECK_CHANNEL")
-                .unwrap()
-                .parse::<u64>()
-                .expect("Tarkistus kanavan ID:tä ei löytynyt");
+                .expect("Lähetys kanavan ID:tä ei löytynyt .env tiedostosta");
 
-            //Tarkastaa onko kanava oikea
+            //tarkastaa onko kanava oikea
             if message.channel_id == send_channel_id {
+                //tarkistus kanavan id .env tiedostossa
+                let check_channel_id = env::var("CHECK_CHANNEL")
+                    .unwrap()
+                    .parse::<u64>()
+                    .expect("Tarkistus kanavan ID:tä ei löytynyt .env tiedostosta");
+
                 let check_channel = ChannelId::from(check_channel_id);
 
                 //Lähetä hakemus tarkistettavaksi
@@ -36,18 +42,120 @@ impl EventHandler for Handler {
                             e.title(format!("Hakemus - {}", message.author.tag()));
                             e.description(format!("```{}```", message.content));
                             e
+                        });
+                        m.components(|c| {
+                            c.create_action_row(|r| {
+                                r.create_button(|b| {
+                                    b.label("Hyväksy");
+                                    b.style(ButtonStyle::Success);
+                                    b.custom_id("hyvaksy_hakemus")
+                                });
+                                r.create_button(|b| {
+                                    b.label("Hylkää");
+                                    b.style(ButtonStyle::Danger);
+                                    b.custom_id("hylkaa_hakemus")
+                                })
+                            })
                         })
                     })
                     .await
                     .unwrap()
                     .id;
 
+                //tallentaa hakemuksen tietoja databaseen
                 save_allowlist(Allowlist {
-                    message_id: msg_id.to_string(),
-                    author_id: message.author.id.to_string(),
+                    message_id: msg_id.0,
+                    author_id: message.author.id.0,
                 });
             }
         };
+    }
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        match interaction {
+            Interaction::MessageComponent(button) => {
+                let guild_id = button.guild_id.unwrap().0;
+                let channel_id = button.channel_id.0;
+                let message_id = button.message.id.0;
+
+                if button.data.custom_id == "hyvaksy_hakemus" {
+                    //hakee databasesta hakemuksen tekiän
+                    let allowlist_author_id: u64 = get_allowlist_author(button.message.id.0);
+
+                    //hakee hakemuksen tekiän discordin apista
+                    let allowlist_user = ctx.http.get_user(allowlist_author_id).await.unwrap();
+                    let mut allowlist_member = ctx
+                        .http
+                        .get_member(guild_id, allowlist_author_id)
+                        .await
+                        .unwrap();
+
+                    //roolin id .env tiedostossa
+                    let allowlist_role_id = env::var("ALLOWLIST_ROLE")
+                        .unwrap()
+                        .parse::<u64>()
+                        .expect("Allowlist roolin ID:tä ei löytynyt .env tiedostosta");
+
+                    //odottaa 1 sekunnin ennen jatkamista
+                    sleep(Duration::from_secs(2)).await;
+
+                    //lisää allowlist roolin
+                    allowlist_member
+                        .add_role(&ctx.http, allowlist_role_id)
+                        .await
+                        .unwrap();
+
+                    //lähettää dm hakemus hyväksytty
+                    allowlist_user
+                        .direct_message(&ctx.http, |m| {
+                            m.embed(|e| {
+                                e.description("Hakemuksesi on hyväksytty");
+                                e
+                            })
+                        })
+                        .await
+                        .unwrap();
+
+                    //poistaa viestin kanavalta
+                    ctx.http
+                        .delete_message(channel_id, message_id)
+                        .await
+                        .unwrap();
+
+                    //poistaa datan databasesta
+                    remove_allowlist(message_id);
+                } else if button.data.custom_id == "hylkaa_hakemus" {
+                    //hakee databasesta hakemuksen tekiän
+                    let allowlist_author_id: u64 = get_allowlist_author(button.message.id.0);
+
+                    //hakee discordin apista hakemuksen tekijän
+                    let allowlist_user = ctx.http.get_user(allowlist_author_id).await.unwrap();
+
+                    //odottaa 1 sekunnin ennen jatkamista
+                    sleep(Duration::from_secs(2)).await;
+
+                    //lähettää dm hakemus hylätty
+                    allowlist_user
+                        .direct_message(&ctx.http, |m| {
+                            m.embed(|e| {
+                                e.description("Hakemuksesi on hylätty");
+                                e
+                            })
+                        })
+                        .await
+                        .unwrap();
+
+                    //poistaa viestin kanavalta
+                    ctx.http
+                        .delete_message(channel_id, message_id)
+                        .await
+                        .unwrap();
+
+                    //poistaa datan databasesta
+                    remove_allowlist(message_id);
+                }
+            }
+            _ => {}
+        }
     }
 }
 
@@ -55,9 +163,14 @@ impl EventHandler for Handler {
 async fn main() {
     dotenv::dotenv().ok();
     //discord
-    let bot_token = env::var("BOT_TOKEN").expect("Token not found!");
+    let bot_token = env::var("BOT_TOKEN").expect("Tokenia ei löytynyt .env tiedostosta!");
+    let application_id = env::var("APPLICATION_ID")
+        .unwrap()
+        .parse::<u64>()
+        .expect("Application ID:tä ei löytynyt .env tiedostosta");
 
     let mut client = Client::builder(&bot_token)
+        .application_id(application_id)
         .event_handler(Handler)
         .intents(
             GatewayIntents::GUILD_MESSAGES
@@ -65,9 +178,7 @@ async fn main() {
                 | GatewayIntents::GUILD_INTEGRATIONS,
         )
         .await
-        .expect("Error creating client");
+        .expect("Clienttiä ei voitu luoda");
 
-    if let Err(e) = client.start().await {
-        println!("Client error: {}", e);
-    }
+    client.start().await.unwrap();
 }
